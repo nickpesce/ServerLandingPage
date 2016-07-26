567ddf64010e6ad19848528e45d9220e00ca489f
Nick Pesce <npesce@terpmail.umd.edu>
2016-07-25 22:31:58 -0400

    Used isotope.js to make categorized tiles

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
+app.use(express.static(__dirname));
 
-app.get("", function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile("index.html", {root: __dirname});
 });
 
